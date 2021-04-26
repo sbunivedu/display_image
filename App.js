14062b2dc74adf9126f5b41f1d63e33b1150c323
Baochuan Lu <lubaochuan@gmail.com>
2021-04-26 09:46:09 -0500

    add code and instructions

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image,  StyleSheet, Text, View } from 'react-native';
+import logo from './assets/icon.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+{/* uncomment the following line to display an image from local assets
+        <Image source={logo} style={{ width: 305, height: 159 }} />
+  */}
+      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={{ width: 305, height: 159 }} />
+
+      <Text style={{color: '#888', fontSize: 18}}>
+        To share a photo from your phone with a friend, just press the button below!
+      </Text>
     </View>
   );
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+This project demonstrates how display an image from local assets or a web URL.
+
+source: https://docs.expo.io/tutorial/image/
